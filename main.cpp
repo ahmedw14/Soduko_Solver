23de2e229a2030add8dcbfca23926e1e6c0f8882
ahmedw14 <67707288+ahmedw14@users.noreply.github.com>
2021-09-16 10:36:54 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,97 +1,40 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
 #include <fstream>
+#include <chrono>
 using namespace std;
-#define UNASSIGNED 0
-#define N 9
+#include "solver.h"  // The SudokuSolver class.
 
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
-bool isSafe(int grid[N][N], int row, int col, int num);
-
-/* assign values to all unassigned locations for Sudoku solution  
- */
-bool SolveSudoku(int grid[N][N])
-{
-    int row, col;
-    if (!FindUnassignedLocation(grid, row, col))
-       return true;
-    for (int num = 1; num <= 9; num++)
-    {
-    if (isSafe(grid, row, col, num))
-    {
-        grid[row][col] = num;
-        if (SolveSudoku(grid))
-            return true;
-        grid[row][col] = UNASSIGNED;
-    }
-    }
-   
-    return false;
-}
-
-/* Searches the grid to find an entry that is still unassigned. */
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col)
-{
-    for (row = 0; row < N; row++)
-        for (col = 0; col < N; col++)
-            if (grid[row][col] == UNASSIGNED)
-                return true;
-    return false;
-}
-
-bool isSafe(int grid[N][N], int row, int col, int num)
-{
-   	for (int i=0; i<9;i++) //check row
-		if( grid[row][i]==num )
-			return false;
-			
-	for (int j=0; j < 9;j++) //check column
-		if( grid[j][col]==num )
-			return false;
-	
-	int threebythree_row =row - (row%3);
-	int threebythree_column =  col - (col%3);
-	
-	for (int i = 0; i < 3; i++)   //check_box
-        for (int j = 0; j < 3; j++)
-            if (grid[i+threebythree_row][j+threebythree_column] == num) 
-                return false;
-	
-	
-	return true;
-}
-
-void printGrid(int grid[N][N])
-{
-    for (int row = 0; row < N; row++)
-    {
-        for (int col = 0; col < N; col++)
-            cout<<grid[row][col]<<"  ";
-        cout<<endl;
-    }
-}
-
-int main()
-{
+void check_file (const char *file){
+	int NUM=9;
 	ifstream in_file;
-	in_file.open ("input.txt");
-	string line;
-	int count=0;//to check that there are at least 17 known numbers
-	int grid[N][N];	
-	for (int i=0; i<9; i++){ //Converting user input into a Vector 
-		in_file>>line;
-		for (int j=0; j<9;j++){
-			grid[i][j]=line[j]-'0'; 
-			if (grid[i][j] != 0)
-				count++;	
-		}
-	}
+	in_file.open (file);
 	
-    
-    SolveSudoku(grid);
-    printGrid(grid);
-//cout<<"No solution exists"<<endl;
+	if (in_file.fail()){
+		cout << "Error, Cannot open file" << endl;
+		exit(1);
+	}
+	string line;
+	int i=0;
+	while (getline(in_file,line)){
+		if (i>8|| NUM+1 != int(line.length())){
+			cout << "invalid Soduko input"<< endl;
+			exit(1);
+		}
+	for (int j=0; j<int(line.length())-1;j++){
+		if((!isdigit(line[j]))){
+			cout << "invalid Soduko input"<< endl;
+			exit(1); } 
+		}
+	
+		i++;
+	}
+}
+int main(){
+	//auto start = chrono::steady_clock::now();
+	check_file ("input.txt");
+	solver s("input.txt");
+	//auto end = chrono::steady_clock::now();
+	//auto diff = end - start;
+	//cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
     return 0;
 }
